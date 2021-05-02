4b997fd5dfa442cc7556abbd95638a00af095300
sdfgx123 <sdfgx123@naver.com>
2021-05-03 01:28:47 +0900

    naver

--- SpringPRJ/src/poly/service/impl/MailService.java
+++ SpringPRJ/src/poly/service/impl/MailService.java
@@ -24,7 +24,7 @@ public class MailService implements IMailService {
 	
 	final String host = "smtp.naver.com";
 	final String user = "sdfgx123@naver.com";
-	final String password = "duce%*pp^#76";
+	final String password = "";
 	
 	@Override
 	public int doSendMail(MailDTO pDTO) {
